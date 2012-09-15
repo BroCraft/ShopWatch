539f919292907b6b84b81ca9112eec606b25936a
Brad <Zycro>
2012-09-15 15:23:58 -0500

    Added plugin.yml configuration file and some basic commands in the plugin file

---
+++ ShopWatch/src/brocraft/ShopWatch.java
@@ -0,0 +1,17 @@
+package brocraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShopWatch extends JavaPlugin {
+	@Override
+    public void onEnable(){
+        // TODO Insert logic to be performed when the plugin is enabled
+		getLogger().info("ShopWatch has been enabled!");
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    	getLogger().info("ShopWatch has been disabled!");
+    }
+}
