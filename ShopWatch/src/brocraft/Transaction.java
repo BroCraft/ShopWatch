0c7a30b218b502b23256fa6b0393982b5e7d0d44
Brad <Zycro>
2012-09-15 18:04:46 -0500

    Added transaction class used to represent rows in the database

---
+++ ShopWatch/src/brocraft/Transaction.java
@@ -0,0 +1,57 @@
+package brocraft;
+
+public class Transaction {
+	private int id;
+	private String playerName;
+	private int value;
+	private boolean read;
+	
+	/**
+	 * @return the playerName
+	 */
+	public String getPlayerName() {
+		return playerName;
+	}
+	/**
+	 * @param playerName the playerName to set
+	 */
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the read
+	 */
+	public boolean isRead() {
+		return read;
+	}
+	/**
+	 * @param read the read to set
+	 */
+	public void setRead(boolean read) {
+		this.read = read;
+	}
+}
